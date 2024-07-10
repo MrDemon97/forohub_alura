a2cc12510a80a4a5586cc5cb712b5580af3e22ad
Jair Coral <jaircoral88@gmail.com>
2024-07-10 13:55:54 -0500

    Modelado servicios, UsuarioService

---
+++ forohub/src/main/java/com/alura/forohub/forohub/services/UsuarioService.java
@@ -0,0 +1,28 @@
+package com.alura.forohub.forohub.services;
+
+import com.alura.forohub.forohub.models.Usuario;
+import com.alura.forohub.forohub.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public Usuario registrarUsuario(Usuario usuario){
+        usuario.setContraseña(passwordEncoder.encode(usuario.getContraseña()));
+        return usuarioRepository.save(usuario);
+    }
+
+    public Optional<Usuario> encontrarPorEmail(String email){
+        return usuarioRepository.findByEmail(email);
+    }
+}
