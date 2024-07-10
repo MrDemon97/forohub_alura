99c0c9dbb667c15d4a263c921ee5bc25ea9f65cc
Jair Coral <jaircoral88@gmail.com>
2024-07-10 13:21:06 -0500

    Modelado Entidad Curso

---
+++ forohub/src/main/java/com/alura/forohub/forohub/models/Curso.java
@@ -0,0 +1,32 @@
+package com.alura.forohub.forohub.models;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "cursos")
+public class Curso {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "El nombre es obligatorio")
+    private String nombre;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
