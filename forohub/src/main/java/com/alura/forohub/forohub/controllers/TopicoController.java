68ac64e657ee0ece54cffceec320f086d6561c28
Jair Coral <jaircoral88@gmail.com>
2024-07-10 14:42:07 -0500

    Modelando y modificando controldaores y servicios de entidad topico

--- forohub/src/main/java/com/alura/forohub/forohub/controllers/TopicoController.java
+++ forohub/src/main/java/com/alura/forohub/forohub/controllers/TopicoController.java
@@ -1,4 +1,31 @@
 package com.alura.forohub.forohub.controllers;
 
+import com.alura.forohub.forohub.models.Topico;
+import com.alura.forohub.forohub.services.TopicoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/topicos")
 public class TopicoController {
+
+    @Autowired
+    private TopicoService topicoService;
+
+    @GetMapping
+    public ResponseEntity<Topico> crearTopico(@Validated @RequestBody Topico topico){
+        Topico nuevoTopico = topicoService.crearTopico(topico);
+        return new ResponseEntity<>(nuevoTopico, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminarTopico(@PathVariable Long id){
+        topicoService.eliminarTopico(id);
+        return ResponseEntity.noContent().build();
+    }
 }
