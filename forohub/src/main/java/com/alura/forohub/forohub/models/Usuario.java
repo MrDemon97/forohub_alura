acfeaee5d0273351fee9e5224aad9ae76d477d8c
Jair Coral <jaircoral88@gmail.com>
2024-07-10 12:23:53 -0500

    Modelado Entidad Usuario

--- forohub/src/main/java/com/alura/forohub/forohub/models/Usuario.java
+++ forohub/src/main/java/com/alura/forohub/forohub/models/Usuario.java
@@ -13,5 +13,54 @@ public class Usuario {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank
+    @NotBlank(message = "El nombre es obligatoio")
+    private String nombre;
+
+    @NotBlank(message = "El correo es obligatorio")
+    private String email;
+
+    @NotBlank(message = "La contraseña es obligatoria")
+    private String contraseña;
+
+    private String rol;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
 }
